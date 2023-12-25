7b1d911d946e7e4a85bed1f937ede29eae08c875
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2023-12-25 12:54:40 +0900

    fix multiple driven

--- src/fpga/rtl/sources_1/new/silent/step_calculator.sv
+++ src/fpga/rtl/sources_1/new/silent/step_calculator.sv
@@ -4,7 +4,7 @@
  * Created Date: 21/12/2023
  * Author: Shun Suzuki
  * -----
- * Last Modified: 24/12/2023
+ * Last Modified: 25/12/2023
  * Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
  * -----
  * Copyright (c) 2023 Shun Suzuki. All rights reserved.
@@ -199,10 +199,6 @@ module step_calculator #(
             end
           end
         end
-
-        // wait phase fold
-        is_phase_reset[1] <= is_phase_reset[0];
-        is_phase_reset[2] <= is_phase_reset[1];
 
         if (load_cnt > AddSubLatency + AddSubLatency + 1 + DivLatency) begin
           intensity <= intensity_buf[AddSubLatency+1+AddSubLatency+1+DivLatency];
