fb7417c6623ef11150c6c1712f42334681ddb7ef
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-07 10:50:54 +0900

    fix cpu test

--- src/cpu/tests/app.cpp
+++ src/cpu/tests/app.cpp
@@ -63,17 +63,18 @@ TEST(Op, Slot2) {
 
   Header* header = reinterpret_cast<Header*>(data.data);
   header->msg_id = get_msg_id();
-  header->slot_2_offset = 2;
+  header->slot_2_offset = 14;
 
-  const auto idx = 0x80;
+  const uint8_t ty[4] = {0x00, 0x01, 0x02, 0x03};
+  const uint16_t value[4] = {0x0123, 0x4567, 0x89ab, 0xcdef};
   {
     auto* data_body = reinterpret_cast<uint8_t*>(data.data) + sizeof(Header);
     data_body[0] = TAG_DEBUG;
-    data_body[1] = idx;
+    std::memcpy(&data_body[2], ty, 4);
+    std::memcpy(&data_body[6], value, 8);
   }
   {
-    auto* data_body =
-        reinterpret_cast<uint8_t*>(data.data) + sizeof(Header) + 2;
+    auto* data_body = reinterpret_cast<uint8_t*>(data.data) + sizeof(Header) + 14;
     data_body[0] = TAG_FORCE_FAN;
     data_body[1] = 0x01;
   }
@@ -86,9 +87,15 @@ TEST(Op, Slot2) {
   const auto ack = _sTx.ack >> 8;
   ASSERT_EQ(ack, header->msg_id);
 
-  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_OUT_IDX), idx);
-  ASSERT_TRUE((bram_read_controller(BRAM_ADDR_CTL_FLAG) & CTL_FLAG_FORCE_FAN) ==
-              CTL_FLAG_FORCE_FAN);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_0), ty[0]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_1), ty[1]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_2), ty[2]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_3), ty[3]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_0), value[0]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_1), value[1]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_2), value[2]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_3), value[3]);
+  ASSERT_TRUE((bram_read_controller(BRAM_ADDR_CTL_FLAG) & CTL_FLAG_FORCE_FAN) == CTL_FLAG_FORCE_FAN);
 }
 
 TEST(Op, WDT) {
--- src/cpu/tests/op/debug.cpp
+++ src/cpu/tests/op/debug.cpp
@@ -19,11 +19,13 @@ TEST(Op, ConfigureDebugOutIdx) {
   header->msg_id = get_msg_id();
   header->slot_2_offset = 0;
 
-  const auto idx = 0x80;
+  const uint8_t ty[4] = {0x00, 0x01, 0x02, 0x03};
+  const uint16_t value[4] = {0x0123, 0x4567, 0x89ab, 0xcdef};
 
   auto* data_body = reinterpret_cast<uint8_t*>(data.data) + sizeof(Header);
   data_body[0] = TAG_DEBUG;
-  data_body[1] = idx;
+  std::memcpy(&data_body[2], ty, 4);
+  std::memcpy(&data_body[6], value, 8);
 
   auto frame = to_frame_data(data);
 
@@ -33,5 +35,12 @@ TEST(Op, ConfigureDebugOutIdx) {
   const auto ack = _sTx.ack >> 8;
   ASSERT_EQ(ack, header->msg_id);
 
-  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_OUT_IDX), idx);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_0), ty[0]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_1), ty[1]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_2), ty[2]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_TYPE_3), ty[3]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_0), value[0]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_1), value[1]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_2), value[2]);
+  ASSERT_EQ(bram_read_controller(BRAM_ADDR_DEBUG_VALUE_3), value[3]);
 }
