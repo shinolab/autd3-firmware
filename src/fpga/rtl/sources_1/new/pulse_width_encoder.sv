3001083c0e7a939ee460c04ff7a30889b6a73d0e
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-17 14:27:49 +0900

    refactor pulse_width_encoder module

--- src/fpga/rtl/sources_1/new/pulse_width_encoder.sv
+++ src/fpga/rtl/sources_1/new/pulse_width_encoder.sv
@@ -15,17 +15,14 @@ module pulse_width_encoder #(
   localparam int Latency = 1;
 
   logic [15:0] addr;
-  logic [7:0] dout;
-  logic full_width[3];
+  logic [ 7:0] dout;
 
-  logic [7:0] phase_buf[DEPTH];
-  logic [$clog2(DEPTH):0] phase_set_cnt = 0;
-  logic [7:0] phase_out;
+  logic full_width_in, full_width_out;
 
   logic [8:0] pulse_width_out;
   logic dout_valid;
 
-  logic [$clog2(DEPTH+(Latency+1))-1:0] cnt, set_cnt;
+  logic [$clog2(DEPTH+(2+Latency+1))-1:0] cnt;
 
   typedef enum logic {
     WAITING,
@@ -34,11 +31,28 @@ module pulse_width_encoder #(
 
   state_t state = WAITING;
 
+  delay_fifo #(
+      .WIDTH(1),
+      .DEPTH(2)
+  ) full_width_fifo (
+      .CLK (CLK),
+      .DIN (full_width_in),
+      .DOUT(full_width_out)
+  );
+
+  delay_fifo #(
+      .WIDTH(8),
+      .DEPTH(4)
+  ) phase_fifo (
+      .CLK (CLK),
+      .DIN (PHASE_IN),
+      .DOUT(PHASE_OUT)
+  );
+
   assign DUTY_TABLE_BUS.IDX = addr;
   assign dout = DUTY_TABLE_BUS.VALUE;
 
   assign PULSE_WIDTH_OUT = pulse_width_out;
-  assign PHASE_OUT = phase_out;
   assign DOUT_VALID = dout_valid;
 
   always_ff @(posedge CLK) begin
@@ -46,35 +60,18 @@ module pulse_width_encoder #(
       WAITING: begin
         dout_valid <= 1'b0;
         if (DIN_VALID) begin
-          cnt <= 0;
-          set_cnt <= 0;
-
-          phase_buf[0] <= PHASE_IN;
-          phase_set_cnt <= 1;
-
-          addr <= INTENSITY_IN;
-
+          cnt   <= 0;
+          addr  <= INTENSITY_IN;
           state <= RUN;
         end
       end
       RUN: begin
-        if (phase_set_cnt < DEPTH) begin
-          phase_buf[phase_set_cnt] <= PHASE_IN;
-          phase_set_cnt <= phase_set_cnt + 1;
-        end
-
         addr <= INTENSITY_IN;
-
         cnt  <= cnt + 1;
-
         if (cnt > Latency) begin
           dout_valid <= 1'b1;
-          pulse_width_out <= {full_width[2], dout[7:0]};
-          phase_out <= phase_buf[set_cnt];
-          set_cnt <= set_cnt + 1;
-          if (set_cnt == DEPTH - 1) begin
-            state <= WAITING;
-          end
+          pulse_width_out <= {full_width_out, dout[7:0]};
+          state <= cnt == Latency + DEPTH ? WAITING : state;
         end
       end
       default: begin
@@ -82,10 +79,7 @@ module pulse_width_encoder #(
     endcase
   end
 
-  always_ff @(posedge CLK) begin
-    full_width[0] <= INTENSITY_IN >= PULSE_WIDTH_ENCODER_SETTINGS.FULL_WIDTH_START;
-    full_width[1] <= full_width[0];
-    full_width[2] <= full_width[1];
-  end
+  always_ff @(posedge CLK)
+    full_width_in <= INTENSITY_IN >= PULSE_WIDTH_ENCODER_SETTINGS.FULL_WIDTH_START;
 
 endmodule
