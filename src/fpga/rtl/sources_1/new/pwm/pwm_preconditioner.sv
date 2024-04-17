b450d4b09b9b03af208a5a9af1ef01dbc883d23b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-17 14:23:52 +0900

    refactor pwm_preconditioner module

--- src/fpga/rtl/sources_1/new/pwm/pwm_preconditioner.sv
+++ src/fpga/rtl/sources_1/new/pwm/pwm_preconditioner.sv
@@ -14,137 +14,112 @@ module pwm_preconditioner #(
   localparam int AddSubLatency = 2;
 
   logic [8:0] rise[DEPTH], fall[DEPTH];
-
-  logic [8:0] pulse_width_buf[4], phase_buf;
   logic [8:0] rise_buf[DEPTH], fall_buf[DEPTH];
 
-  logic signed [10:0] a_phase, b_phase, s_phase;
-  logic signed [10:0] a_pulse_width_r, b_pulse_width_r, s_pulse_width_r;
-  logic signed [10:0] a_rise, b_rise, s_rise;
-  logic signed [10:0] a_fall, b_fall, s_fall;
+  logic [8:0] pulse_width_buf;
 
-  logic [$clog2(DEPTH+1+AddSubLatency*2)-1:0] cnt, lr_cnt, set_cnt;
+  logic [8:0] s_phase;
+  logic [7:0] s_pulse_width_r;
+  logic [9:0] s_rise, s_fall;
+
+  logic [$clog2(DEPTH+1+AddSubLatency*2)-1:0] cnt;
 
   logic dout_valid;
 
   assign DOUT_VALID = dout_valid;
+  assign RISE = rise;
+  assign FALL = fall;
 
-  for (genvar i = 0; i < DEPTH; i++) begin : gen_rise_fall
-    assign RISE[i] = rise[i];
-    assign FALL[i] = fall[i];
-  end
+  delay_fifo #(
+      .WIDTH(9),
+      .DEPTH(4)
+  ) pw_buf (
+      .CLK (CLK),
+      .DIN (PULSE_WIDTH),
+      .DOUT(pulse_width_buf)
+  );
 
   addsub #(
-      .WIDTH(11)
+      .WIDTH(9)
   ) sub_phase (
       .CLK(CLK),
-      .A  (a_phase),
-      .B  (b_phase),
+      .A  (9'd256),
+      .B  ({1'b0, PHASE}),
       .ADD(1'b0),
       .S  (s_phase)
   );
   addsub #(
-      .WIDTH(11)
+      .WIDTH(8)
   ) add_pulse_width_r (
       .CLK(CLK),
-      .A  (a_pulse_width_r),
-      .B  (b_pulse_width_r),
+      .A  ({PULSE_WIDTH[8:1]}),
+      .B  ({7'h00, PULSE_WIDTH[0]}),
       .ADD(1'b1),
       .S  (s_pulse_width_r)
   );
 
   addsub #(
-      .WIDTH(11)
+      .WIDTH(10)
   ) sub_rise (
       .CLK(CLK),
-      .A  (a_rise),
-      .B  (b_rise),
+      .A  ({s_phase, 1'b0}),
+      .B  ({2'b00, pulse_width_buf[8:1]}),
       .ADD(1'b0),
       .S  (s_rise)
   );
   addsub #(
-      .WIDTH(11)
+      .WIDTH(10)
   ) add_fall (
       .CLK(CLK),
-      .A  (a_fall),
-      .B  (b_fall),
+      .A  ({s_phase, 1'b0}),
+      .B  ({2'b00, s_pulse_width_r}),
       .ADD(1'b1),
       .S  (s_fall)
   );
 
   typedef enum logic [2:0] {
-    WAITING,
+    IDLE,
+    WAIT0,
+    WAIT1,
     RUN,
     DONE
   } state_t;
 
-  state_t state = WAITING;
+  state_t state = IDLE;
 
   always_ff @(posedge CLK) begin
     case (state)
-      WAITING: begin
+      IDLE: begin
         dout_valid <= 1'b0;
         if (DIN_VALID) begin
-          cnt <= '0;
-          lr_cnt <= '0;
-          set_cnt <= '0;
-
-          state <= RUN;
+          cnt   <= '0;
+          state <= WAIT0;
         end
       end
+      WAIT0: begin
+        state <= WAIT1;
+      end
+      WAIT1: begin
+        state <= RUN;
+      end
       RUN: begin
-        // step 1
-        a_phase <= 11'd512;
-        b_phase <= {2'b00, phase_buf};
-        a_pulse_width_r <= {3'b000, pulse_width_buf[0][8:1]};
-        b_pulse_width_r <= pulse_width_buf[0][0];
         cnt <= cnt + 1;
-
-        // step 2
-        a_rise <= s_phase;
-        b_rise <= {3'b000, pulse_width_buf[1+AddSubLatency][8:1]};
-        a_fall <= s_phase;
-        b_fall <= s_pulse_width_r;
-        if (cnt > AddSubLatency) begin
-          lr_cnt <= lr_cnt + 1;
-        end
-
-        if (lr_cnt > AddSubLatency) begin
-          rise_buf[set_cnt] <= s_rise[8:0];
-          fall_buf[set_cnt] <= s_fall[8:0];
-
-          set_cnt <= set_cnt + 1;
-        end
-
-        if (set_cnt == DEPTH - 1) begin
-          state <= DONE;
-        end
+        rise_buf[cnt] <= s_rise[8:0];
+        fall_buf[cnt] <= s_fall[8:0];
+        state <= (cnt == AddSubLatency + AddSubLatency + DEPTH - 1) ? DONE : state;
       end
       DONE: begin
         dout_valid <= 1'b1;
-
-        state <= WAITING;
+        state <= IDLE;
       end
-      default: begin
-      end
+      default: state <= IDLE;
     endcase
   end
 
   always_ff @(posedge CLK) begin
-    pulse_width_buf[0] <= PULSE_WIDTH;
-    pulse_width_buf[1] <= pulse_width_buf[0];
-    pulse_width_buf[2] <= pulse_width_buf[1];
-    pulse_width_buf[3] <= pulse_width_buf[2];
-
-    phase_buf <= {PHASE, 1'b0};
-  end
-
-  for (genvar i = 0; i < DEPTH; i++) begin : gen_copy_buf
-    always_ff @(posedge CLK) begin
-      if (state == DONE) begin
-        rise[i] <= rise_buf[i];
-        fall[i] <= fall_buf[i];
-      end
+    if (state == DONE) begin
+      rise <= rise_buf;
+      fall <= fall_buf;
     end
   end
 
