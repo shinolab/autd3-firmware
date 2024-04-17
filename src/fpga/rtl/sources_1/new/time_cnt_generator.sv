5dc0e80e9a53927d309c03c79e6d43ef877a5b43
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-04-17 14:28:00 +0900

    refactor time_cnt_generator module

--- src/fpga/rtl/sources_1/new/time_cnt_generator.sv
+++ src/fpga/rtl/sources_1/new/time_cnt_generator.sv
@@ -11,25 +11,17 @@ module time_cnt_generator #(
   logic [8:0] t_buf0, t_buf1, t;
   logic update;
 
-  assign t_buf0   = SYS_TIME[8:0];
+  wire  cross0 = t_buf0 == 9'd0;
+  wire  cross0_with_skip = (t_buf0 == 9'd1) & SKIP_ONE_ASSERT;
 
+  assign t_buf0   = SYS_TIME[8:0];
   assign UPDATE   = update;
   assign TIME_CNT = t;
-
-  logic cross0;
-  assign cross0 = t_buf0 == 9'd0;
-
-  logic cross0_with_skip;
-  assign cross0_with_skip = (t_buf0 == 9'd1) & SKIP_ONE_ASSERT;
 
   always_ff @(posedge CLK) begin
     t_buf1 <= t_buf0;
     t <= t_buf1;
-    if (cross0 | cross0_with_skip) begin
-      update <= 1'b1;
-    end else begin
-      update <= 1'b0;
-    end
+    update <= cross0 | cross0_with_skip;
   end
 
 endmodule
