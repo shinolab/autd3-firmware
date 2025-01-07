8784e18dd0b5e5d3f7654e0a707da4214138404a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-07 13:18:47 +0900

    fix clk.c

--- src/cpu/tests/op/clk.cpp
+++ src/cpu/tests/op/clk.cpp
@@ -16,7 +16,6 @@ TEST(Op, Clk) {
   std::memset(data.data, 0, sizeof(RX_STR));
 
   Header* header = reinterpret_cast<Header*>(data.data);
-  header->msg_id = get_msg_id();
   header->slot_2_offset = 0;
 
   uint64_t rom[32] = {0};
@@ -24,20 +23,36 @@ TEST(Op, Clk) {
 
   auto* data_body = reinterpret_cast<uint8_t*>(data.data) + sizeof(Header);
   data_body[0] = TAG_CONFIG_FPGA_CLK;
-  data_body[1] = CLK_FLAG_BEGIN | CLK_FLAG_END;
-  *reinterpret_cast<uint16_t*>(data_body + 2) = 32;
-  for (size_t i = 0; i < 32; i++)
-    reinterpret_cast<uint64_t*>(data_body + 4)[i] = rom[i];
 
-  auto frame = to_frame_data(data);
+  {
+    header->msg_id = get_msg_id();
+    data_body[1] = CLK_FLAG_BEGIN;
+    *reinterpret_cast<uint16_t*>(data_body + 2) = 16;
+    for (size_t i = 0; i < 16; i++) reinterpret_cast<uint64_t*>(data_body + 4)[i] = rom[i];
 
-  recv_ethercat(&frame[0]);
-  update();
+    auto frame = to_frame_data(data);
 
-  const auto ack = _sTx.ack >> 8;
-  ASSERT_EQ(ack, header->msg_id);
+    recv_ethercat(&frame[0]);
+    update();
 
-  for (size_t i = 0; i < 128; i++) {
-    ASSERT_EQ(bram_read_clk(i), reinterpret_cast<uint16_t*>(rom)[i]);
+    const auto ack = _sTx.ack >> 8;
+    ASSERT_EQ(ack, header->msg_id);
   }
+
+  {
+    header->msg_id = get_msg_id();
+    data_body[1] = CLK_FLAG_END;
+    *reinterpret_cast<uint16_t*>(data_body + 2) = 16;
+    for (size_t i = 0; i < 16; i++) reinterpret_cast<uint64_t*>(data_body + 4)[i] = rom[16 + i];
+
+    auto frame = to_frame_data(data);
+
+    recv_ethercat(&frame[0]);
+    update();
+
+    const auto ack = _sTx.ack >> 8;
+    ASSERT_EQ(ack, header->msg_id);
+  }
+
+  for (size_t i = 0; i < 128; i++) ASSERT_EQ(bram_read_clk(i), reinterpret_cast<uint16_t*>(rom)[i]);
 }
